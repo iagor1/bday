eba414865a5a783b435bc7fee384ab2733138114
Iagor M.L <iagorluchesi@gmail.com>
2023-03-31 20:09:54 -0400

    final commit

---
+++ src/Card.jsx
@@ -0,0 +1,22 @@
+export default () => (
+  <div className="birthdayCard">
+    <div className="cardFront">
+      <h3 className="happy">PAREBENS ANAB!</h3>
+      <div className="balloons">
+        <div className="balloonOne" />
+        <div className="balloonTwo" />
+        <div className="balloonThree" />
+        <div className="balloonFour" />
+      </div>
+    </div>
+    <div className="cardInside">
+      <h3 className="back">PARABENS ANAB!</h3>
+      <p>Querida querida,</p>
+      <p>
+        Feliz aniver querida lov u S2
+        beijos linda muac
+      </p>
+      <p className="name">Pawan</p>
+    </div>
+  </div>
+);
